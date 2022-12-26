f3fec6df77f44f95c179f36b71ca14ef38fcc142
phong <phongndsaiyan@gmail.com>
2022-12-26 16:23:15 +0700

    deleted uneccesaryfiles

--- ccxt_sample.py
+++
@@ -1,9 +0,0 @@
-import ccxt
-
-exchange = ccxt.binance()
-
-markets = exchange.load_markets()
-print(exchange.parse_ohlcv("BTC/USD"))
-# print(exchange.describe()["has"]["fetchOHLCV"])
-# for item in (exchange.describe()):
-#     print(item)
